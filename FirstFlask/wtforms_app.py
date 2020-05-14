efbd93482a219cf9cbadc2ba4c83c8fb884d7f6c
Lexxkit <lexxkit@gmail.com>
2020-05-14 12:16:20 +0300

    Add wtforms_app.py with validation; add hidden_tag to form.html

--- FirstFlask/wtforms_app.py
+++ FirstFlask/wtforms_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms.validators import InputRequired, Email
 
 app = Flask(__name__)
 app.secret_key = 'randomstring'
@@ -8,8 +9,9 @@ app.secret_key = 'randomstring'
 
 # создаем форму
 class OrderForm(FlaskForm):
-    name = StringField('Имя')
+    name = StringField('Имя', validators=[InputRequired()])
     phone = StringField('Телефон')
+    mail = StringField('Email', validators=[Email()])
 
 
 '''Создание формы и прием данных в разных представлениях
@@ -41,15 +43,17 @@ def render_form():
     form = OrderForm()
 
     # If data was sent
-    if request.method == 'POST':
+    # if request.method == 'POST' and form.validate():
+    if form.validate_on_submit():
         # get data
         name = form.name.data
         phone = form.phone.data
+        mail = form.mail.data
 
         # render data
-        return render_template('save.html', name=name, phone=phone)
+        return render_template('save.html', name=name, phone=phone, mail=mail)
 
-    #If no data was sent
+    # If no data was sent
     return render_template('form.html', form=form)
 
 app.run(debug=True)
